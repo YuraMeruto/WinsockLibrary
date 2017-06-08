ae869c46bf7bb2504a7f2be4c46adf0fc2925c5e
YUUTO\大樹 <yakmsy.memories@ezweb.ne.jp>
2017-06-09 08:45:28 +0900

    TCPのサーバへのconnect通信の作成完了

--- Client 2/Project1/Project1/main.cpp
+++ Client 2/Project1/Project1/main.cpp
@@ -1,46 +1,62 @@
-#include <stdio.h>
-#include <winsock2.h>
-#include<winsock.h>
-#include<stdlib.h>
-#include<WS2tcpip.h>
+//#include <stdio.h>
+//#include <winsock2.h>
+//#include<winsock.h>
+//#include<stdlib.h>
+//#include<WS2tcpip.h>
 //#include<thread.h>
-#include"Test.h"
+
+#include"WinSock.h"
 //配列の場所渡し
 int main()
 {
-
 	Winsock win =  Winsock(2,0);
-	//win.GetMyIPAdress();
+	win.TCPInstanceSocet();
+	int PortNumber =12345;
+	win.TCPServerConect("127.0.0.1",PortNumber);
+	win.TCPFromServerRecv();
+	win.PrintRecv();
 	system("pause");
-	/*
-	WSADATA wsaData;
-	struct sockaddr_in server;
-	SOCKET sock;
-	int x = 12345;
-	char buf[32];
-
-	// winsock2の初期化
-	WSAStartup(MAKEWORD(2, 0), &wsaData);
-
-	// ソケットの作成
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-
-	// 接続先指定用構造体の準備
-	inet_pton(AF_INET,"127.0.0.1",&server.sin_addr);
-	server.sin_family = AF_INET;
-	server.sin_port = htons(x);
-	// サーバに接続
-	connect(sock, (struct sockaddr *)&server, sizeof(server));
-
-	// サーバからデータを受信
-	memset(buf, 0, sizeof(buf));
-	int n = recv(sock, buf, sizeof(buf),0);
-
-	printf("%d, %s\n", n, buf);
-
-	// winsock2の終了処理
-	WSACleanup();
-	system("pause");
-	return 0;
-	*/
 }
+
+
+
+
+
+
+
+
+
+
+
+//win.GetMyIPAdress();
+/*
+WSADATA wsaData;
+struct sockaddr_in server;
+SOCKET sock;
+int x = 12345;
+char buf[32];
+
+// winsock2の初期化
+WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+// ソケットの作成
+sock = socket(AF_INET, SOCK_STREAM, 0);
+
+// 接続先指定用構造体の準備
+inet_pton(AF_INET,"127.0.0.1",&server.sin_addr);
+server.sin_family = AF_INET;
+server.sin_port = htons(x);
+// サーバに接続
+connect(sock, (struct sockaddr *)&server, sizeof(server));
+
+// サーバからデータを受信
+memset(buf, 0, sizeof(buf));
+int n = recv(sock, buf, sizeof(buf),0);
+
+printf("%d, %s\n", n, buf);
+
+// winsock2の終了処理
+WSACleanup();
+system("pause");
+return 0;
+*/
